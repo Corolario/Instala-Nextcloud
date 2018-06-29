358a64a27d804a11651763415133a9559e27f359
Corolário <21142386+Corolario@users.noreply.github.com>
2018-06-29 12:11:32 -0300

    Create variaveis.sh

---
+++ variaveis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+###############
+## variaveis ##
+###############
+
+dominio=algo.dominio.com
+
+bd_nome=nextcloud
+bd_usuario=usuario
+bd_senha=senha
+
+adm_usuario=ncadmin
+adm_senha=ncadmin987
