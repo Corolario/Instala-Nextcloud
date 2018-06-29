8e6847c7c5192c6aee0c9580408607d62430ec49
Corolário <21142386+Corolario@users.noreply.github.com>
2018-06-29 10:31:57 -0300

    Update Instala-Nextcloud.sh

--- Instala-Nextcloud.sh
+++ Instala-Nextcloud.sh
@@ -13,27 +13,30 @@ a2enmod rewrite
 a2enmod headers
 
 apt install php7.0-mysql php7.0-zip php7.0-xml php7.0-mbstring php7.0-gd php7.0-curl -y
-apt install php7.0-bz2 php7.0-intl php7.0-mcrypt
+#apt install php7.0-bz2 php7.0-intl php7.0-mcrypt
 
 wget https://download.nextcloud.com/server/releases/nextcloud-13.0.4.tar.bz2
 tar -xf nextcloud-13.0.4.tar.bz2
 cp -r nextcloud /var/www/nextcloud
 chown -R www-data:www-data /var/www/
 
-echo "Alias /nextcloud "/var/www/nextcloud/"
+#echo "Alias /nextcloud "/var/www/nextcloud/"
+#
+#Directory /var/www/nextcloud/>
+#  Options +FollowSymlinks
+#  AllowOverride All
+#
+# <IfModule mod_dav.c>
+#  Dav off
+# </IfModule>
+#
+# SetEnv HOME /var/www/nextcloud
+# SetEnv HTTP_HOME /var/www/nextcloud
+#
+#</Directory>" >> /etc/apache2/sites-available/nextcloud.conf
 
-<Directory /var/www/nextcloud/>
-  Options +FollowSymlinks
-  AllowOverride All
-
- <IfModule mod_dav.c>
-  Dav off
- </IfModule>
-
- SetEnv HOME /var/www/nextcloud
- SetEnv HTTP_HOME /var/www/nextcloud
-
-</Directory>" >> /etc/apache2/sites-available/nextcloud.conf
+wget https://raw.githubusercontent.com/Corolario/Instala-Nextcloud/master/ubu_ssl.conf -O /etc/apache2/site-available/
+wget https://raw.githubusercontent.com/Corolario/Instala-Nextcloud/master/ubu_dom.conf -O /etc/apache2/site-available/
 
 a2ensite nextcloud
 
@@ -44,8 +47,8 @@ GRANT ALL PRIVILEGES ON nxtcloud.* TO 'usuario'@'localhost';
 quit;
 CRIA_BD
 
-apt-get install php-apcu
-apt-get install redis-server php-redis
+#apt-get install php-apcu
+#apt-get install redis-server php-redis
 
 #sudo -u www-data php occ config:system:set trusted_domains 0 --value=IP_EXTERNO
 service apache2 restart
